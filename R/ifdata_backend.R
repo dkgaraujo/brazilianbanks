df319689d1f884ad42462a1855cf5e74d14cea64
Douglas K. G. Araujo <dkg.araujo@gmail.com>
2022-04-27 23:12:05 +0200

    Include Apache 2.0 license

--- R/ifdata_backend.R
+++ R/ifdata_backend.R
@@ -1,3 +1,17 @@
+# Copyright 2022 Douglas Kiarelly Godoy de Araujo
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #' Downloads information about the IF.Data reports starting from yyyymm_start and the last available one if yyyymm_end is NULL
 #'
 #' @inheritParams get_bank_stats
