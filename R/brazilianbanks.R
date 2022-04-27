a25b91df704a6e6733610fc5f79044921f201815
Douglas K. G. Araujo <dkg.araujo@gmail.com>
2022-04-27 23:11:11 +0200

    License and text corrections

--- R/brazilianbanks.R
+++ R/brazilianbanks.R
@@ -1,4 +1,19 @@
-#' WIP WIP WIP brazilianbanks: Data on Brazilian banks and other regulated financial institutions
+# Copyright 2022 Douglas Kiarelly Godoy de Araujo
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#' brazilianbanks: Data on Brazilian banks and other regulated financial institutions
 #'
 #' A user-friendly interface to official datasets of bank-level variables
 #' sourced from the Central Bank of Brazil, in particular the 'IF.data' dataset.
@@ -8,7 +23,7 @@
 #' This package uses two data sources: the 'IF.data' system maintained by the Central Bank of Brazil (BCB in the Portuguese acronym) and banks' Pillar 3 regulatory disclosures, which is regulated by the BCB but maintained and populated by each bank.
 #'
 #' @section Functions:
-#' The functions are...
+#' The functions are `available_quarters()` and `get_bank_stats()`.
 #'
 #' @docType package
 #' @name brazilianbanks
