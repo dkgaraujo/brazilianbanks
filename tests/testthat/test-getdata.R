08f613112938bc52b7eaca9452c1c788a7637ef6
Douglas Araujo <dkg.araujo@gmail.com>
2022-08-19 03:14:39 +0200

    New test on finding data for each quarter

--- tests/testthat/test-getdata.R
+++ tests/testthat/test-getdata.R
@@ -3,3 +3,13 @@ test_that("the code finds the JSON list of files from the BCB site", {
   expect_equal("https://www3.bcb.gov.br/ifdata/rest/relatorios", json_path)
   expect_equal(TRUE, RJSONIO::isValidJSON(json_path)) # weirdly, this endpoint passes RJSONIO validation, but not jsonlite validation.
 })
+
+test_that("data are findable for a reasonable range of dates", {
+  yyyymm_start <- 201403
+  yyyymm_end <- 202109
+  num_qtrs <- length(brazilianbanks:::all_quarters_between(yyyymm_start = yyyymm_start,
+                                                           yyyymm_end = yyyymm_end))
+
+  num_qtrs_info <- length(brazilianbanks:::download_IFdata_reports(yyyymm_start, yyyymm_end, cache_json = FALSE))
+  expect_equal(num_qtrs, num_qtrs_info)
+})
