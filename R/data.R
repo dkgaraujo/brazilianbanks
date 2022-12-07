e6a957642b2e6313cfaa639c3f1fc2075c70d754
Douglas Araujo <dkg.araujo@gmail.com>
2022-12-07 01:38:03 +0100

    Data documentation

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Brazilian banks and other financial institutions: 2017 Q1 to 2021 Q4
+#'
+#' Accounting, risk and other information for banks and
+#' other financial institutions in Brazil, from the
+#' 'IF.data' system of the Central Bank of Brazil.
+#'
+#' @format ## `brazilian_banks_201703_202112`
+#' A tibble with 26,885 rows and 426 columns
+#'
+#' @source https://www3.bcb.gov.br/ifdata/
+"brazilian_banks_201703_202112"
